7c89cb4f005e1c57d015ffd1c9c3c35f4cfc531b
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-04-21 10:14:09 +0200

    added spectral plot to speasy_utils

--- speasy_utils/plot.py
+++ speasy_utils/plot.py
@@ -317,4 +317,60 @@ def plot_scores(scores, metrics):
     
         ax.set_xlim(0,1)
     return fig
+
+def spectro_plot(param_id, start, stop, ylabel=None, channels = None, ax=None, figsize=(10,2), 
+                 vmin=None, vmax=None, lognorm=True):
+    """Plot 'spectral data'.
+    """
+    if ax is None:
+        fig, ax = plt.subplots(1,1,figsize=figsize)
+    # get the data
+    param_data = spz.get_data(param_id, start, stop)
+    [n,m] = param_data.data.shape
+    X = param_data.data   
+    
+    # channels (constant channels case)
+    if channels is None:
+        y = np.arange(0,m,1)
+    else:
+        y = channels
+    
+    # grid
+    x1, y1 = np.meshgrid(param_data.time,y, indexing="ij")
+    
+    # data bounds
+    if vmin is None:
+        vmin = np.nanmin(X)
+    if vmax is None:
+        vmax = np.nanmax(X)
+    
+    # colormap
+    cmap = matplotlib.cm.rainbow.copy()
+    cmap.set_bad('White',0.)
+    
+    # normalize colormapping
+    if lognorm and vmin>0.:
+        norm=colors.LogNorm(vmin=vmin, vmax=vmax)
+    else:
+        norm=None
+    
+    
+    c = ax.pcolormesh(x1, y1, X, cmap=cmap, norm=norm, edgecolors="face")
+    plt.colorbar(c,ax=ax, norm=norm)
+    
+    ax.set_xlabel("Time")
+    x_ticks = ax.get_xticks()
+    x_ticks = [datetime.utcfromtimestamp(xi) for xi in x_ticks]
+    x_labels = [d.strftime("%H:%M") for d in x_ticks]
+    
+    ticks_loc = ax.get_xticks().tolist()
+    ax.xaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
+    ax.set_xticklabels(x_labels)
+    
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    
+    ax.set_ylim(y.min(), y.max())
+    
+    return ax
     
